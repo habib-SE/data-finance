c3f84466e09f37cd5de9d7aca8fd8ff3f6ceac0e
habib-jani <hab67603@gmail.com>
2023-10-08 23:41:43 +0500

    add comment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,8 @@ import {
   FaTwitterSquare,
 } from 'react-icons/fa';
 
+// Footer Section
+
 const Footer = () => {
   return (
     <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
